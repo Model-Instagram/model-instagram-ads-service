87a0da086a27cafbc9c17308da42283650430554
Adam Miller <chadam189@gmail.com>
2017-12-11 17:55:39 -0800

    Set up airbnb linter

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+/**
+ * These rules enforce the Airbnb Style Guide
+ *
+ * Visit this repo for more information:
+ *   https://github.com/airbnb/javascript
+ */
+
+module.exports = {
+  "extends": ["airbnb-standard"],
+  "env": {
+    "browser": true,
+    "node": true
+  },
+};
