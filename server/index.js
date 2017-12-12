86711c4f19535e633c3946d524fb97b1bc47f985
Adam Miller <chadam189@gmail.com>
2017-12-11 17:45:37 -0800

    Set up express server and .gitignore

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.listen(8080, () => console.log('Interactions server is listening on port 8080!'));
