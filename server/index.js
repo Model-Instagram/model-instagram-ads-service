bb04b973014719b5480b956bfed87fca96620219
Adam Miller <chadam189@gmail.com>
2017-12-19 17:14:39 -0800

    Install newRelic, add testing suite, refactor helper files

--- server/index.js
+++ server/index.js
@@ -1,49 +1,48 @@
-// const apm = require('elastic-apm-node').start({
-//   // Set required app name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
-//   appName: 'instagram-ads',
-//   // Use if APM Server requires a token
-//   secretToken: '',
-//   // Set custom APM Server URL (default: http://localhost:8200)
-//   serverUrl: '',
-// });
 const NR = require('newrelic');
+const Promise = require('bluebird');
 
 const express = require('express');
-const { getNextAd } = require('../utils/getNextAd.js');
-const { recordInteraction } = require('../utils/recordInteractions.js');
-const { seedAllData } = require('../database/seed.js');
+const {
+  getNextAd,
+  recordInteraction,
+  incrementLikeCount,
+  updateFriendLikes,
+} = require('../utils/helpers.js');
 
-seedAllData();
+// const { seedAllData } = require('../database/seed.js');
+// seedAllData();
 
 const app = express();
-// app.use(apm.middleware.express());
 
 // serve up next ad for a user's feed
 app.get('/users/:user_id/ad_feed/:next_ad_index', (req, res) => {
-  const userId = req.params.user_id;
-  const nextAdIndex = req.params.next_ad_index;
+  const userId = parseInt(req.params.user_id, 10);
+  const nextAdIndex = parseInt(req.params.next_ad_index, 10);
   getNextAd(userId, nextAdIndex)
     .then((result) => {
       res.send(result);
     })
-    .catch((error) => {
-      console.log(error);
-      res.sendStatus(500);
-    });
+    .catch(error => console.log(error));
+});
+
+app.get('/testHTTP', (req, res) => {
+  res.sendStatus(200);
 });
 
 // handle ad likes
 app.post('/likes/ads/:ad_id/users/:user_id', (req, res) => {
   const userId = parseInt(req.params.user_id, 10);
   const adId = parseInt(req.params.ad_id, 10);
-  recordInteraction(userId, adId, 'like')
-    .then(() => {
+  Promise.all([
+    recordInteraction(userId, adId, 'like'),
+    incrementLikeCount(adId),
+    updateFriendLikes(userId, adId),
+  ])
+    .then((results) => {
+      console.log('done with ad like', results);
       res.sendStatus(200);
     })
-    .catch((error) => {
-      console.log(error);
-      res.sendStatus(500);
-    });
+    .catch(error => console.log(error));
 });
 
 // handle ad views
@@ -54,10 +53,7 @@ app.post('/views/ads/:ad_id/users/:user_id', (req, res) => {
     .then(() => {
       res.sendStatus(200);
     })
-    .catch((error) => {
-      console.log(error);
-      res.sendStatus(500);
-    });
+    .catch(error => reject(error));
 });
 
 // handle ad clicks
@@ -68,10 +64,11 @@ app.post('/clicks/ads/:ad_id/users/:user_id', (req, res) => {
     .then(() => {
       res.status(200);
     })
-    .catch((error) => {
-      console.log(error);
-      res.sendStatus(500);
-    });
+    .catch(error => reject(error));
 });
 
+app.all('*', (req, res) => res.sendStatus(404));
+
 app.listen(8080, () => console.log('Ads server is listening on port 8080!'));
+
+module.exports = app;
